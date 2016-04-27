224a6445d91645f2560838f3aa0a84527600379a
Christina Nguyen <christina.nguyen.cn@gmail.com>
2016-04-27 09:56:46 -0400

    paperclip num of arguments

--- app/models/link.rb
+++ app/models/link.rb
@@ -4,7 +4,7 @@ class Link < ActiveRecord::Base
 	acts_as_votable
 	belongs_to :user
 	has_many :comments
-	has_attached_file :picture, :image, styles: { medium: "300x300", thumb: "150x150#" },
+	has_attached_file :image, styles: { medium: "300x300", thumb: "150x150#" },
 	storage: :s3,
 	:bucket => 'wyncode-techit',
 	:s3_credentials => {
@@ -18,8 +18,6 @@ class Link < ActiveRecord::Base
 		posts = posts.sort{|a,b| b.get_upvotes.size <=> a.get_upvotes.size}
 	end
 
-	def picture_from_url(url)
-    self.picture = open(url)
-  	end
+
 end
 
