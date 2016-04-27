e4d266d56e6293b0ad01b557f56701437f4ecec5
Christina Nguyen <christina.nguyen.cn@gmail.com>
2016-04-26 23:22:36 -0400

    interpolationserror

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,7 +2,7 @@ class Link < ActiveRecord::Base
 	acts_as_votable
 	belongs_to :user
 	has_many :comments
-	has_attached_file :image, :url => ":s3_domain_url", styles: { medium: "300x300", thumb: "150x150#" }
+	has_attached_file :image, :url => :s3_domain_url, styles: { medium: "300x300", thumb: "150x150#" }
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
 	def self.sort_by_upvotes(posts)
